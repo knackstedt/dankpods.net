dbda363914ff5cca8f4c08b5a7f2557e89242913
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-12-06 22:49:55 -0500

    Reflow links for mobile and add a privacy policy disclaimer

--- src/app/root.component.ts
+++ src/app/root.component.ts
@@ -1,10 +1,19 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Fetch } from 'src/app/services/fetch.service';
-
-import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environment';
 import { KeyboardService } from './services/keyboard.service';
 import { WallpaperService } from './services/wallpaper.service';
+
+// import Swiper core and required modules
+import SwiperCore, { Lazy, Pagination, Navigation, Virtual, Keyboard,  } from "swiper";
+import { MatDrawer } from '@angular/material/sidenav';
+import { MatDialog } from '@angular/material/dialog';
+import { AboutComponent } from './dialogs/about/about.component';
+
+// install Swiper modules
+SwiperCore.use([Lazy, Pagination, Navigation, Virtual, Keyboard]);
+
+const desktopWidth = 1126;
 
 
 @Component({
@@ -14,16 +23,49 @@ import { WallpaperService } from './services/wallpaper.service';
     encapsulation: ViewEncapsulation.None
 })
 export class RootComponent {
+    @ViewChild("drawer") drawer: MatDrawer;
     environment = environment;
 
     taskbarPosition: "top" | "right" | "bottom" | "left" = "left";
 
+    readonly headLinks = [
+        { url: "https://www.youtube.com/c/DankPods", label: "YouTube" },
+        { url: "https://www.patreon.com/dankpods", label: "Patreon" },
+        { url: "https://www.floatplane.com/channel/GarbageTime/home", label: "Floatplane" },
+        { url: "https://www.instagram.com/biteyfrank", label: "Frank" },
+        { url: "https://discord.gg/BrssHgh6jP", label: "Discord" },
+        { url: "https://www.youtube.com/channel/UCHdpnvKJDijKNe2caIasnww", label: "Garbage Time" },
+        { url: "https://www.youtube.com/@thedrumthing4665", label: "The Drum Thing" },
+        { url: "https://www.youtube.com/channel/UCfITAHFPUbFwCbMYrhMJJCw", label: "Dankmus" },
+    ]
+
     constructor(
         private fetch: Fetch,
-        public dialog: MatDialog,
         private keyboard: KeyboardService,
-        public wallpaper: WallpaperService
+        public wallpaper: WallpaperService,
+        private dialog: MatDialog
     ) {
+        this.onResize();
+    }
 
+    onButtonClick(link) {
+        window.location.href = link.url;
+    }
+
+    openInfo() {
+        this.dialog.open(AboutComponent);
+    }
+
+
+    isDesktop = true;
+    @HostListener("window:resize", ["$event"])
+    onResize() {
+        if (window.innerWidth >= desktopWidth)
+            this.isDesktop = true;
+        else
+            this.isDesktop = false;
+
+        if (this.isDesktop)
+            this.drawer.close()
     }
 }
--- src/app/root.module.ts
+++ src/app/root.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { RootComponent } from './root.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material.module';
 import { PortalModule } from '@angular/cdk/portal';
 import { CommonModule } from '@angular/common';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environment';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { RootComponent } from './root.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
     declarations: [
@@ -18,11 +23,15 @@ import { environment } from '../environment';
         CommonModule,
         BrowserModule,
         BrowserAnimationsModule,
-        MaterialModule,
         PortalModule,
         HttpClientModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatDialogModule,
+        MatIconModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
-          enabled: environment.production,
+          enabled: !isDevMode(),
           // Register the ServiceWorker as soon as the application is stable
           // or after 30 seconds (whichever comes first).
           registrationStrategy: 'registerWhenStable:30000'
