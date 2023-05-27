d9fc7e44f2a7a9010c3807f8453dcb5b548c7456
GoCD Automation <no-reply@dotglitch.dev>
2023-05-26 20:19:13 -0400

    Cleanup the About page

--- src/app/dialogs/about/about.component.ts
+++ src/app/dialogs/about/about.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  imports: [
+    MatIconModule,
+    MatButtonModule
+  ],
+  standalone: true
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor(public dialog: MatDialogRef<any>) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
